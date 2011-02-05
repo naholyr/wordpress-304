d1081c490a6af62bd54bdf878c1babb8e35d9b83
Nicolas Chambrier <naholyr@gmail.com>
2011-02-05 23:55:06 +0100

    Support for just-posted comments making comment author's local cache expire.

--- 304-not-modified.php
+++ 304-not-modified.php
@@ -27,7 +27,18 @@ class WPHeader304Manager
 			add_filter('wp_headers', array($this, 'setLastModifiedHeader'), 10, 2);
 			// Check request, compare modified dates, and throw a 304 status if possible
 			add_action('send_headers', array($this, 'check304'));
+			// When user posts comment, store this date in a cookie so that we can expire his cache
+			add_filter('comment_post_redirect', array($this, 'storeLastCommentPosted'), 10, 2);
 		}
+	}
+
+	function storeLastCommentPosted($location, $comment)
+	{
+		if ($comment) {
+			$comment_cookie_lifetime = apply_filters('comment_cookie_lifetime', 30000000);
+			setcookie('comment_date_' . COOKIEHASH, $comment->comment_date, time() + $comment_cookie_lifetime, COOKIEPATH, COOKIE_DOMAIN);
+		}
+		return $location;
 	}
 
 	protected $headers = null;
@@ -40,6 +51,9 @@ class WPHeader304Manager
 			// Retrieve last post modified, not depending on type (includes standard posts, pages, but also any future type of post)
 			$wp_last_modified_date = $wpdb->get_var("SELECT GREATEST(post_modified_gmt, post_date_gmt) d FROM $wpdb->posts WHERE post_status = 'publish' ORDER BY d DESC LIMIT 1");
 			$wp_last_modified_date = max($wp_last_modified_date, get_lastcommentmodified('GMT'));
+			if ($user_comment_date = $_COOKIE['comment_date_' . COOKIEHASH]) {
+				$wp_last_modified_date = max($wp_last_modified_date, $user_comment_date);
+			}
 			$wp_last_modified = mysql2date('D, d M Y H:i:s', $wp_last_modified_date, 0) . ' GMT';
 			$headers['Last-Modified'] = $wp_last_modified;
 			$headers['ETag'] = '"' . md5($wp_last_modified) . '"';
